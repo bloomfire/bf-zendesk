bf82959aaa7946c78030218c16ba00e5e173efdc
Troy Warr <troy3000@gmail.com>
2017-02-21 20:51:43 -0600

    minor copy change; equalize Linked Resources title lengths

--- src/components/AccessMessageUnsupported.js
+++ src/components/AccessMessageUnsupported.js
@@ -8,7 +8,7 @@ import WarningIcon from './WarningIcon';
 const AccessMessageUnsupported = (props) => (
   <aside className="access-message access-message-unsupported">
     <WarningIcon/>
-    <h3>This version of the Zendesk app<br/>is no longer supported by Bloomfire.</h3>
+    <h3>This version of the app<br/>is no longer supported by Bloomfire.</h3>
     <p>Please upgrade to the newest version of the app<br/>in order to continue using it.</p>
   </aside>
 );
